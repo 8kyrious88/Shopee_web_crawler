9494d5ba9d52a600b0844bbca177f93ded269a49
ncp <chunping071141@gmail.com>
2026-02-19 16:56:27 +0800

    First Submission

---
+++ main.py
@@ -0,0 +1,78 @@
+import os, time, random
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
+def human_scroll(driver):
+    # Get the total height of the page
+    total_height = driver.execute_script("return document.body.scrollHeight")
+    current_pos = 0
+    
+    while current_pos < total_height:
+        # Scroll down by a random amount (200 to 600 pixels)
+        step = random.randint(200, 600)
+        current_pos += step
+        driver.execute_script(f"window.scrollTo(0, {current_pos});")
+        
+        # Random "thinking" pause
+        time.sleep(random.uniform(0.5, 1.5))
+        
+        # Update height in case the page expanded (infinite scroll)
+        total_height = driver.execute_script("return document.body.scrollHeight")
+
+
+load_dotenv()
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+user_data_path = os.getenv('USER_DATA_PATH')
+chrome_options = uc.ChromeOptions()
+chrome_options.add_argument(f'--user-data-dir={user_data_path}')
+chrome_options.add_argument('--profile-directory=Default')
+driver = uc.Chrome(options=chrome_options, version_main=144)
+driver.get("https://shopee.sg/top_products?catId=SG_BITL0_1068%3Atop_sold")
+time.sleep(3)
+human_scroll(driver=driver)
+
+html_content = driver.page_source
+driver.quit()
+soup = BeautifulSoup(html_content,'html.parser')
+with open('scrapped_page.html','w', encoding='utf-8') as file:
+    file.write(soup.prettify())
+
+
+### Normall account log in
+# original_window = driver.current_window_handle
+# wait = WebDriverWait(driver, 10)
+# username_column = wait.until(EC.visibility_of_element_located((By.NAME,'loginKey')))
+# for char in username:
+#     username_column.send_keys(char)
+#     time.sleep(random.uniform(0.05,0.2))
+# password_column = driver.find_element(By.NAME,'password')
+# for char in password:
+#     password_column.send_keys(char)
+#     time.sleep(random.uniform(0.05,0.2))
+# time.sleep(random.uniform(1,4))
+
+# login_button = driver.find_element(By.XPATH,'//*[@id="main"]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/form/button')
+# login_button.click()
+# time.sleep(5)
+
+
+### Google account log in
+# button_google_login = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main"]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/div[3]/div[2]/button[2]')))
+# time.sleep(3)
+# button_google_login.click()
+# time.sleep(3)
+# driver.switch_to.window(driver.window_handles[-1])
+# email_column = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="identifierId"]')))
+# for char in 'chunping071141@gmail.com':
+#     email_column.send_keys(char)
+#     time.sleep(random.uniform(0.05,0.2))
+# time.sleep(1)
+# login_button = driver.find_element(By.XPATH,'//*[@id="identifierNext"]/div/button')
+# login_button.click()
+# driver.switch_to.window(original_window)
+# time.sleep(10)
